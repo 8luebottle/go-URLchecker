1cb5287459ba644763eac3611cbae04441d3f7e7
8luebottle <itbiz.irs@gmail.com>
2020-02-07 18:48:48 +0900

    WIP : Adding Go Channel

--- main.go
+++ main.go
@@ -4,23 +4,18 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
+
+type result struct {
+	url    string
+	status string
+}
 
 var errRequestFailed = errors.New("Request Failed!")
 
 func main() {
-	// Testing Go Channel
-	c := make(chan string)
-	people := [5]string{"Baby", "Tiger", "8luebottle", "Blue", "She"}
-	for _, person := range people {
-		go isSexy(person, c)
-	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c) //  Loop : len(people)
-	}
+	c := make(chan result)
 
-	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -31,33 +26,18 @@ func main() {
 		"https://www.instagram.com",
 		"http://babytiger.netlify.com/",
 	}
-	results["hello"] = "Hello"
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
+		go hitURL(url, c)
 	}
 }
 
-// Check one by one --> checking like Python
-// Later, for concurrency I'm going to use goroutine
-func hitURL(url string) error {
-	fmt.Println("Checking URL: ", url)
+// Beautiful Go's Concurrency
+func hitURL(url string, c chan<- result) { // Channel that send only
+	fmt.Println("Checking: ", url)
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
-	return nil
-}
-
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 10)
-	c <- person + " is Sexy" // Send "true" to the Channel c
+	c <- result{url: url, status: status}
 }
