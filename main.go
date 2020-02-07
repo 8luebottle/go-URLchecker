1db6278b030af0d8196e63b9f761a43373d26192
8luebottle <itbiz.irs@gmail.com>
2020-02-07 17:34:54 +0900

    Slow URL Chekcer
    hitURL() function act like Python. I'm going to use goroutine.

--- main.go
+++ main.go
@@ -9,25 +9,38 @@ import (
 var errRequestFailed = errors.New("Request Failed!")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.apple.com/",
 		"https://www.amazon.com/",
-		"https://reddit.com",
+		"https://www.reddit.com",
 		"https://www.facebook.com",
 		"https://www.instagram.com",
 		"http://babytiger.netlify.com/",
 	}
+	results["hello"] = "Hello"
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
+// Check one by one --> checking like Python
+// Later, for concurrency I'm going to use goroutine
 func hitURL(url string) error {
 	fmt.Println("Checking URL: ", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
